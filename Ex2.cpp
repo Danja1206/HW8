eae57fa7496e332eae41733a2779202338def900
Danja1206 <105879524+Danja1206@users.noreply.github.com>
2022-06-24 15:38:22 +0300

    Add files via upload

---
+++ Ex2.cpp
@@ -0,0 +1,56 @@
+﻿#include <iostream>
+#include <exception>
+
+using namespace std;
+
+class Ex
+{
+public:
+    Ex(double _x) : x(_x) {}
+
+    double what() const noexcept { return x; };
+
+private:
+    double x;
+};
+
+class Bar
+{
+public:
+    Bar() : y(0) {}
+
+    void set(double a)
+    {
+        if (y + a > 100) throw Ex(a * y);
+        else y = a;
+    }
+
+private:
+    double y;
+};
+
+int main()
+{
+    setlocale(LC_ALL, "rus");
+    Bar bar;
+    double n;
+    try
+    {
+        do
+        {
+            cin >> n;
+            bar.set(n);
+            cout << endl;
+
+        } while (n != 0);
+    }
+    catch (Ex& ex)
+    {
+        cerr << "Ошибка: Ex с параметром - " << ex.what() << endl;
+    }
+    catch (...)
+    {
+        cerr << "Ошибка" << endl;
+    }
+}
+
